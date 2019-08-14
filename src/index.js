401131f21de2f611b4c92d3063c287cea8b1ccf3
Cam <cameronedwards145@gmail.com>
2019-08-14 19:42:06 +0100

    Added basic player movement

--- src/index.js
+++ src/index.js
@@ -6,13 +6,45 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const context = canvas.getContext("2d");
 
-var x = 0;
+const player = {
+    x: 32,
+    y: 32,
+    width: 32,
+    height: 32,
+    speed: 3
+};
+
+keys = {};
+
+window.onkeydown = function(event) {
+    keys[event.keyCode] = true;
+};
+
+window.onkeyup = function(event) {
+    keys[event.keyCode] = false;
+}
+
 function tick(elapsed) {
-    context.fillStyle = "rgb(" + x + "," + x + "," + x + ")";
-    context.fillRect(0, 0, WIDTH, HEIGHT);
-    x = (x + 1) % 255;
-    
+    update(elapsed);
+    draw(context);
     window.requestAnimationFrame(tick);
+}
+
+function update(delta) {
+    if (keys[65]) {
+        player.x -= player.speed;
+    }
+    else if (keys[68]) {
+        player.x += player.speed;
+    }
+}
+
+function draw(context) {
+    context.fillStyle = "blue";
+    context.fillRect(0, 0, WIDTH, HEIGHT);
+
+    context.fillStyle = "white";
+    context.fillRect(player.x, player.y, player.width, player.height);
 }
 
 tick(0);
