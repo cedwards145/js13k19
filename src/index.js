7b8d27dedfbd52b0d2b32d09b88f0b6ece537b3f
Cam <cameronedwards145@gmail.com>
2019-08-14 20:19:08 +0100

    Added basic blobs

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const context = canvas.getContext("2d");
 
-const player = new Player(32, 32);
+const player = new Player(600, HEIGHT - 32);
+const blobs = [];
+for (var count = 0; count < 50; count++) {
+    const size = Math.random() * 64 + 16;
+    const blob = new Player(Math.random() * 200, HEIGHT - size);
+    blob.width = size;
+    blob.height = size;
+    blob.speed = Math.random() * 1.75 + 1;
+    blobs.push(blob);
+}
 
 const keys = {};
 
@@ -35,14 +44,29 @@ function update(delta) {
         direction = 1;
     }
     player.move(direction);
+
+    for (var index = 0; index < blobs.length; index++) {
+        const blob = blobs[index];
+        const direction = Math.sign(player.x - blob.x);
+        blob.move(direction);
+    }
 }
 
 function draw(context) {
-    context.fillStyle = "blue";
+    context.fillStyle = "CornflowerBlue";
     context.fillRect(0, 0, WIDTH, HEIGHT);
 
     context.fillStyle = "white";
     context.fillRect(player.x, player.y, player.width, player.height);
+
+    context.fillStyle = "black";
+    for (var index = 0; index < blobs.length; index++) {
+        const blob = blobs[index];
+        context.beginPath();
+        context.ellipse(blob.x + (blob.width / 2), blob.y + (blob.height / 2), blob.width / 2, blob.height / 2, 0, 0, 360);
+        context.closePath();
+        context.fill();
+    }
 }
 
 tick(0);
